291f0e1df06760159921893eb45de2244c72e99d
kelseybcoding <kelseybcoding@gmail.com>
2017-03-04 13:55:29 -0600

    added thumbnail images to movie index page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,10 @@
 class Movie < ApplicationRecord
   has_many :showtimes
+
+  validates :title, :rating, :summary, :run_time, :image_url, presence: true #checks that these fields aren't empty
+  validates :title, uniqueness: true #checks that the title is unique among all movies
+  validates :image_url, allow_blank: true, format: {  #makes sure image URL looks reasonable.
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be URL for GIF, JPG, or PNG image.' 
+  }
 end
